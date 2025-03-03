9a3a690e994e0f776bf9804bc21392569a778953
leo <leo@confluent-digital.com>
2025-03-03 17:47:03 +0100

    fix: start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install the Olfactis API
-cd api && npm install && docker compose up -d && npm run dev && cd ..
+cd backend && npm install && docker compose up -d && npm run dev && cd ..
 
 # Install the Olfactis application
-cd olfactis && npm install && npm run dev && cd ..
+cd frontend && npm install && npm run dev && cd ..
