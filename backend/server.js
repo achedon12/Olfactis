cc7d59aa0e259c3043b59a63f7a470c0e0afabe5
IPandragonI <mathysf84pro@gmail.com>
2025-03-05 15:41:01 +0100

    Add a start sh for me

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,6 @@ app.use(cors());
 app.use('/api/auth', require('./routes/Auth'));
 app.use('/api/user', require('./routes/User'));
 
-// api for bouquet
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
