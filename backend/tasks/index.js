ecd067e9630410dced931a3717f9fb89b0258e37
Achedon12 <leoderoinleo@gmail.com>
2025-06-16 09:34:09 +0200

    feat: reminder cron

--- backend/tasks/index.js
+++ backend/tasks/index.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const sendReturnReminder = require('./Reminder');
 
-
+// Schedule a task to run at 8 AM on the first day of every month
 cron.schedule('0 8 1 * *', async () => {
     await sendReturnReminder();
 }, {
