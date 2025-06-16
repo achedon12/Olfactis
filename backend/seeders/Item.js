88a82c2b5099aff0443ef9eeb4606752039101d6
IPandragonI <mathysf84pro@gmail.com>
2025-06-16 10:38:35 +0200

    feat: fix img

--- backend/seeders/Item.js
+++ backend/seeders/Item.js
@@ -8,7 +8,7 @@ const seedItems = async () => {
         {
             name: 'Copper Alembic',
             description: 'A traditional alembic used for distilling essential oils and plant extracts.',
-            picture: 'copper_alembic.png',
+            picture: '/items/copper_alembic.png',
             reference: 'AL-001',
             category: (await Category.findOne({ name: 'Distillation' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -16,7 +16,7 @@ const seedItems = async () => {
         {
             name: 'Glass Distillation Column',
             description: 'A column used for separating different volatile components in fragrances.',
-            picture: 'glass_distillation_column.png',
+            picture: '/items/glass_distillation_column.png',
             reference: 'DC-001',
             category: (await Category.findOne({ name: 'Distillation' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -26,7 +26,7 @@ const seedItems = async () => {
         {
             name: 'Soxhlet Extractor',
             description: 'An extractor used for obtaining essential oils through solvent extraction.',
-            picture: 'soxhlet_extractor.png',
+            picture: '/items/soxhlet_extractor.png',
             reference: 'SE-001',
             category: (await Category.findOne({ name: 'Extraction' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -34,7 +34,7 @@ const seedItems = async () => {
         {
             name: 'Cold Press Extractor',
             description: 'Used to extract essential oils from citrus peels through mechanical pressing.',
-            picture: 'cold_press_extractor.png',
+            picture: '/items/cold_press_extractor.png',
             reference: 'CP-001',
             category: (await Category.findOne({ name: 'Extraction' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -42,7 +42,7 @@ const seedItems = async () => {
         {
             name: 'Enfleurage Frame',
             description: 'A traditional tool for capturing delicate floral scents using fat absorption.',
-            picture: 'enfleurage_frame.png',
+            picture: '/items/enfleurage_frame.png',
             reference: 'EF-001',
             category: (await Category.findOne({ name: 'Extraction' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -52,7 +52,7 @@ const seedItems = async () => {
         {
             name: 'Gas Chromatograph',
             description: 'A scientific instrument used to analyze the chemical composition of perfumes.',
-            picture: 'gas_chromatograph.png',
+            picture: '/items/gas_chromatograph.png',
             reference: 'GC-001',
             category: (await Category.findOne({ name: 'Analysis' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -60,7 +60,7 @@ const seedItems = async () => {
         {
             name: 'Mass Spectrometer',
             description: 'Used to precisely identify the odorant molecules in a fragrance.',
-            picture: 'mass_spectrometer.png',
+            picture: '/items/mass_spectrometer.png',
             reference: 'MS-001',
             category: (await Category.findOne({ name: 'Analysis' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -68,7 +68,7 @@ const seedItems = async () => {
         {
             name: 'pH Meter',
             description: 'A digital instrument used to measure the pH of liquid solutions in formulations.',
-            picture: 'ph_meter.png',
+            picture: '/items/ph_meter.png',
             reference: 'PH-001',
             category: (await Category.findOne({ name: 'Analysis' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -78,7 +78,7 @@ const seedItems = async () => {
         {
             name: 'Precision Scale',
             description: 'A high-accuracy scale for weighing raw materials in perfume formulation.',
-            picture: 'precision_scale.png',
+            picture: '/items/precision_scale.png',
             reference: 'PS-001',
             category: (await Category.findOne({ name: 'Measurement' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -86,7 +86,7 @@ const seedItems = async () => {
         {
             name: 'Hydrometer',
             description: 'An instrument used to measure the alcohol concentration in perfume mixtures.',
-            picture: 'hydrometer.png',
+            picture: '/items/hydrometer.png',
             reference: 'HY-001',
             category: (await Category.findOne({ name: 'Measurement' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -96,7 +96,7 @@ const seedItems = async () => {
         {
             name: 'Magnetic Stirrer',
             description: 'A device used to mix liquid solutions homogeneously in perfume production.',
-            picture: 'magnetic_stirrer.png',
+            picture: '/items/magnetic_stirrer.png',
             reference: 'MS-002',
             category: (await Category.findOne({ name: 'Mixing' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -104,7 +104,7 @@ const seedItems = async () => {
         {
             name: 'Ultrasonic Homogenizer',
             description: 'Used to break down and mix fragrance molecules evenly in a solution.',
-            picture: 'ultrasonic_homogenizer.png',
+            picture: '/items/ultrasonic_homogenizer.png',
             reference: 'UH-001',
             category: (await Category.findOne({ name: 'Mixing' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -114,7 +114,7 @@ const seedItems = async () => {
         {
             name: 'Graduated Cylinders Set',
             description: 'A set of glass measuring cylinders for precise liquid volume measurement.',
-            picture: 'graduated_cylinders_set.png',
+            picture: '/items/graduated_cylinders_set.png',
             reference: 'GCY-001',
             category: (await Category.findOne({ name: 'Glassware' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -122,7 +122,7 @@ const seedItems = async () => {
         {
             name: 'Pipette Set',
             description: 'A set of precision pipettes used for measuring and transferring liquid fragrance components.',
-            picture: 'pipette_set.png',
+            picture: '/items/pipette_set.png',
             reference: 'PIP-001',
             category: (await Category.findOne({ name: 'Glassware' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -130,7 +130,7 @@ const seedItems = async () => {
         {
             name: 'Erlenmeyer Flask Set',
             description: 'A set of flasks used for mixing and storing liquid perfume formulations.',
-            picture: 'erlenmeyer_flask_set.png',
+            picture: '/items/erlenmeyer_flask_set.png',
             reference: 'EF-002',
             category: (await Category.findOne({ name: 'Glassware' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -140,7 +140,7 @@ const seedItems = async () => {
         {
             name: 'Fragrance Testing Strips',
             description: 'Absorbent paper strips used for testing and evaluating perfume compositions.',
-            picture: 'fragrance_testing_strips.png',
+            picture: '/items/fragrance_testing_strips.png',
             reference: 'FTS-001',
             category: (await Category.findOne({ name: 'Testing' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -148,7 +148,7 @@ const seedItems = async () => {
         {
             name: 'Perfume Bottling Machine',
             description: 'A semi-automatic machine for filling perfume bottles with precise measurements.',
-            picture: 'perfume_bottling_machine.png',
+            picture: '/items/perfume_bottling_machine.png',
             reference: 'PBM-001',
             category: (await Category.findOne({ name: 'Packaging' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
@@ -156,7 +156,7 @@ const seedItems = async () => {
         {
             name: 'Bottle Capping Machine',
             description: 'A machine used to securely seal perfume bottles with caps or spray heads.',
-            picture: 'bottle_capping_machine.png',
+            picture: '/items/bottle_capping_machine.png',
             reference: 'BCM-001',
             category: (await Category.findOne({ name: 'Packaging' })),
             state: (await State.findOne({ name: 'AVAILABLE' }))
